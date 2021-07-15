2cc5c6fb5eae462eea9dead9c68f7421389d414a
KFC <chris.chan@greenpeace.org>
2021-07-15 15:31:59 +0800

    updated path

--- mc-deploy.js
+++ mc-deploy.js
@@ -32,7 +32,7 @@ const FTPS = require("ftps");
 
 // definitions
 let buildFolder = path.join(__dirname, "build"),
-  EndpointURL = "https://cloud.greenhk.greenpeace.org/petition-pp",
+  EndpointURL = "https://cloud.greenhk.greenpeace.org/websign",
   CampaignId = "7012u000000Ou6vAAC",
   DonationPageUrl = "https://www.greenpeace.org/eastasia/", // not used now
   interests = ["Plastics"], // Arctic, Climate, Forest, Health, Oceans, Plastics
